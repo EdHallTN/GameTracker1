8393fde5b405e27c737907b9520ee21d381ae380
John Jastrow <jjastrow>
2016-04-15 16:58:12 -0500

    initial

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+/**
+ * Created by johnjastrow on 4/15/16.
+ */
+public class Game {
+    String name;
+    String genre;
+    String platform;
+    int releaseYear;
+
+    public Game(String name, String genre, String platform, int releaseYear) {
+        this.name = name;
+        this.genre = genre;
+        this.platform = platform;
+        this.releaseYear = releaseYear;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/**
+ * Created by johnjastrow on 4/15/16.
+ */
+public class User {
+    String name;
+    ArrayList<Game> games = new ArrayList<>();
+
+    public User(String name) {
+        this.name = name;
+    }
+}
